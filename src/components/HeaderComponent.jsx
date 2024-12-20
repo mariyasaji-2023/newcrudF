5b0a25205915c90735e4b533a043e06e898f3362
viswanathr73 <viswanathr73@gmail.com>
2024-12-21 00:00:41 +0530

    added logo and fav icon

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link ,useNavigate } from "react-router-dom";
-import logo from "/images/logo3.webp"
+import logo from "/images/hungrx-logo.webp"
 
 const Header = () => {
   const navigate = useNavigate();
